25a566c5943eb49e76eaf7eeadd0024d27a504fa
Rento <amouchrif2@gmail.com>
2025-01-27 23:51:14 +0100

    dvc added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
 AUTHOR_USER_NAME = "ayoubmouchrif"
